2cdf3c2cc9a56892c8d6050c244c856cb13b8a44
brauliopy-lap <balcantararomo@BRAULIOLAP>
2026-06-24 17:00:12 -0600

    se configuro la variable url que usa la api y se instalo router para movernos entre paginas y saber que componentes usar, ademas de que se cargo como componente inicial el main y se creo el index para hacer el ruteo despues

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,9 @@
-//
+import { createApp } from "vue";
+import axios from "axios";
+import App from "./App.vue";
+
+//Usaré esta url para manejar las peticiones con axios que es la que abre mi puerto
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+window.axios = axios;
+createApp(App).mount('#app');
