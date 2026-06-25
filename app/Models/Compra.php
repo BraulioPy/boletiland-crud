52e1156ac4126662bc83acbab6b947815d8416ea
brauliopy-lap <balcantararomo@BRAULIOLAP>
2026-06-25 08:33:47 -0600

    se termino la estructura inicial para mostrar las compras, se procedera con el cambio de los endpoints  y controllers

--- app/Models/Compra.php
+++ app/Models/Compra.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Compra extends Model
 {
     protected $fillable = ['nombre_comprador', 'producto_id', 'cantidad'];
+    protected $appends = ['total'];
 
     public function producto()
     {
         return $this->belongsTo(Producto::class);
+    }
+    public function total(){
+        return $this->cantidad * $this->producto->precio;
     }
 }
--- resources/js/services/compraService.js
+++ resources/js/services/compraService.js
@@ -1,17 +1,19 @@
 export const compraService = {
-    getAll(busquedaData = '', pagina = 1) {
-        return axios.get(`/api/productos?busqueda=${busquedaData}&page=${pagina}`);
+    getAll(busquedaData = "", pagina = 1) {
+        return axios.get(
+            `/api/compras?busqueda=${busquedaData}&page=${pagina}`,
+        );
     },
     getById(id) {
-        return axios.get(`/api/productos/${id}`);
+        return axios.get(`/api/compras/${id}`);
     },
     create(data) {
-        return axios.post('/api/productos', data);
+        return axios.post("/api/compras", data);
     },
     update(id, data) {
-        return axios.put(`/api/productos/${id}`, data);
+        return axios.put(`/api/compras/${id}`, data);
     },
     delete(id) {
-        return axios.delete(`/api/productos/${id}`);
+        return axios.delete(`/api/compras/${id}`);
     },
 };
