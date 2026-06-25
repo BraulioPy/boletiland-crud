805aa70e6108adbd289c63494675e66feeec91d5
brauliopy-lap <balcantararomo@BRAULIOLAP>
2026-06-24 22:54:45 -0600

    Se agrego lo necesario para la busqueda en productos y se termino su componente

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,7 @@
 <script setup></script>
 <template>
-    <div class="bg-zinc-50">
-        <div
-            class="w-full max-w-7xl mx-auto pt-4 px-6 h-screen max-h-150 flex flex-col"
-        >
+    <div class="bg-zinc-50 min-h-screen pb-6">
+        <div class="w-full max-w-7xl mx-auto pt-4 px-6 flex flex-col">
             <nav
                 class="flex items-center gap-10 p-4 bg-zinc-100 border border-zinc-200 rounded-t-lg"
             >
@@ -29,11 +27,11 @@
             </nav>
 
             <div
-                class="p-6 flex-1 overflow-y-auto bg-white border-x border-b border-zinc-200 rounded-b-lg"
+                class="p-6 bg-white border-x border-b border-zinc-200 rounded-b-lg"
             >
                 <div class="space-y-4">
                     <main
-                        class="p-4 max-w bg-zinc-50 rounded border border-zinc-200"
+                        class="p-4 w-full bg-zinc-50 rounded border border-zinc-200"
                     >
                         <RouterView class="items-center"></RouterView>
                     </main>
